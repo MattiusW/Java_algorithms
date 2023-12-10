c4ef7682b0f246be8e255899a8a02c52a9fe6bc4
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-10 07:32:19 +0100

    Add graph function

---
+++ Graph.java
@@ -0,0 +1,29 @@
+public class Graph {
+    
+    int[][] matrix;
+
+    Graph(int size){
+        matrix = new int[size][size];
+    }
+
+    public void addNode(Node node){
+
+    }
+
+    public void addEdge(int src, int dst){
+        matrix[src][dst] = 1;
+    }
+
+    public boolean checkEdge(int src, int dst){
+        if(matrix[src][dst] == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }   
+    
+    public void print(){
+
+    }
+}
